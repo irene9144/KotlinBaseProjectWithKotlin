2b608fcaaf0fc006c4e74ef9142ccb732970445a
sm_kwon <sm_kwon@atecmobility.com>
2024-07-19 14:31:10 +0900

    Dependency 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
 dependencies {
 
     // Java language implementation
-    implementation ("androidx.navigation:navigation-fragment:2.3.3")
-    implementation ("androidx.navigation:navigation-ui:2.3.3")
+//    implementation ("androidx.navigation:navigation-fragment:2.3.3")
+//    implementation ("androidx.navigation:navigation-ui:2.3.3")
 
     // Kotlin
     implementation ("androidx.navigation:navigation-fragment-ktx:2.3.3")
@@ -64,6 +64,7 @@ dependencies {
     // Room database
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.databinding:library:3.2.0-alpha11")
+
 //    implementation("androidx.databinding:databinding-runtime:8.5.0")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
@@ -85,13 +86,17 @@ dependencies {
     //okttp3
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
     //navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
 
     //for view model
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.activity:activity-ktx:1.1.0")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
--- app/src/main/java/com/example/kotlinbaseprojectwithnavigation/view/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbaseprojectwithnavigation/view/ui/activity/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.kotlinbaseprojectwithnavigation.view.ui
 
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kotlinbaseprojectwithnavigation.R
 import com.example.kotlinbaseprojectwithnavigation.base.BaseActivity
 import com.example.kotlinbaseprojectwithnavigation.databinding.ActivityMainBinding
 import com.example.kotlinbaseprojectwithnavigation.viewmodel.MainViewModel
+import com.google.android.material.navigationrail.NavigationRailView
 
 class MainActivity :
     BaseActivity<ActivityMainBinding, MainViewModel>({ ActivityMainBinding.inflate(it) }) {
@@ -44,6 +47,14 @@ class MainActivity :
         mBinding.bottomNavigation.setupWithNavController(navController)
         mBinding.sideNavigation.setupWithNavController(navController)
 
+//
+//        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+//        val navigationRailView: NavigationRailView = mBinding.navigationRail
+//        NavigationUI.setupWithNavController(navigationRailView, navController)
+//
+//        navigationRailView.headerView?.setOnClickListener {
+//            Toast.makeText(this, "Replace this with your own action", Toast.LENGTH_LONG).show()
+//        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
